69e265164f02a23c5133efa50f8cdc31557f6f50
Steve Phillips <elimisteve@gmail.com>
2012-09-26 15:41:33 -0700

    Added RandStrOfLen()

---
+++ random.go
@@ -0,0 +1,12 @@
+// Steve Phillips / elimisteve
+// 2012.09.26
+
+package fun
+
+func RandStrOfLen(length int, charset string) (str string) {
+	for i := 0; i < length; i++ {
+		ndx := rand.Intn(len(charset))
+		str += charset[ndx]
+	}
+	return
+}
