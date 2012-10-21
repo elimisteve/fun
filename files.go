9d8aee4280a94f258861013387cf6b9414f5b495
Steve Phillips <elimisteve@gmail.com>
2012-10-21 03:03:45 -0700

    Added OpenAndAppend()

--- files.go
+++ files.go
@@ -1,6 +1,7 @@
 package fun
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -25,4 +26,21 @@ func MaybeFatalAt(where string, err error) {
 		log.Fatalf("Error near %s: %v\n", where, err)
 	}
 	return
+}
+
+// OpenAndAppend opens the file with the given filename and appends
+// the given string
+func OpenAndAppend(filename string, toAppend string) error {
+	// Open file with given filename w/append-only permissions; create
+	// it if it doesn't already exist
+	flags := os.O_APPEND | os.O_WRONLY | os.O_CREATE
+	file, err := os.OpenFile(filename, flags, 0666)
+	if err != nil {
+		return fmt.Errorf("Error opening log file: %v", err)
+	}
+	defer file.Close()
+	if _, err = file.Write([]byte(toAppend)); err != nil {
+		return fmt.Errorf("Error writing to log file: %v", err)
+	}
+	return nil
 }
