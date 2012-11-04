5b6da535aaa735bcf4762dfc44b969383288333c
Steve Phillips <elimisteve@gmail.com>
2012-11-04 10:22:25 -0800

    Added jsonrpc_types.go, which includes structs used for JSON-RPC v1

---
+++ jsonrpc_types.go
@@ -0,0 +1,17 @@
+// Steve Phillips / elimisteve
+// 2012.04.29
+// Originally part of Decentra prototype
+
+package fun
+
+type JsonRpc1Request struct {
+	Method string        `json:"method"`
+	Params []interface{} `json:"params"`
+	Id     interface{}   `json:"id"`
+}
+
+type JsonRpc1Response struct {
+	Result interface{} `json:"result"`
+	Error  error       `json:"error"`
+	Id     interface{} `json:"id"`
+}
