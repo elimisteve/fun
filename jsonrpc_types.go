e21798860d65b3d643f7d8ca922f696f875c730c
Steve Phillips <elimisteve@gmail.com>
2013-01-13 17:44:39 -0800

    Added ClientRequest and ClientResponse from the standard library's jsonrpc package http://golang.org/src/pkg/net/rpc/jsonrpc/client.go

--- jsonrpc_types.go
+++ jsonrpc_types.go
@@ -4,6 +4,10 @@
 
 package fun
 
+import (
+	"encoding/json"
+)
+
 type JsonRpc1Request struct {
 	Method string        `json:"method"`
 	Params []interface{} `json:"params"`
@@ -14,4 +18,18 @@ type JsonRpc1Response struct {
 	Result interface{} `json:"result"`
 	Error  interface{} `json:"error"`
 	Id     interface{} `json:"id"`
+}
+
+// From http://golang.org/src/pkg/net/rpc/jsonrpc/client.go
+
+type ClientRequest struct {
+	Method string         `json:"method"`
+	Params [1]interface{} `json:"params"`
+	Id     uint64         `json:"id"`
+}
+
+type ClientResponse struct {
+	Result *json.RawMessage `json:"result"`
+	Error  interface{}      `json:"error"`
+	Id     uint64           `json:"id"`
 }
