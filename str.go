ddeeb2a172a3f77f51baf477718ff9ffec5f4247
Steve Phillips <steve@tryingtobeawesome.com>
2015-02-24 11:54:22 -0800

    Fixed bug in RandomString

--- str.go
+++ str.go
@@ -15,7 +15,7 @@ func init() {
 // RandomString produces a string of length `length` using characters
 // from `alphabet`
 func RandomString(alphabet string, length int) string {
-	s := make([]byte, len(alphabet))
+	s := make([]byte, length)
 	alphabetLen := len(alphabet)
 	for i := 0; i < length; i++ {
 		s[i] = alphabet[rand.Intn(alphabetLen)]
